de0aab6bddce0045e1f5f66a3ff82d89c210f64c
Doug Richardson <dougie.richardson@gmail.com>
2014-10-03 22:29:52 -0700

    Sampler takes process directory parameter, moves files there once tcpdump rotates them. Added sanity checks to prevent runaway growth if accumulator isn't reading files.

--- sampler.sh
+++ sampler.sh
@@ -2,9 +2,12 @@
 set -e
 
 usage() {
-    echo "Usage: sampler.sh <interface> <capture_dir>"
+    echo "Usage: sampler.sh <interface> <capture_dir> <process_dir>"
 }
 
+#
+# Get parameters
+#
 if [ -z "$1" ]; then
     echo "Missing interface"
     usage
@@ -19,10 +22,60 @@ if [ -z "$2" ]; then
 fi
 CAPTURE_DIR=$2
 
+if [ -z "$3" ]; then
+    echo "Missing process_dir"
+    usage
+    exit 1
+fi
+PROCESS_DIR=$3
+
+#
+# Sanity checks
+#
+if [ $(readlink -f $CAPTURE_DIR) == "/" ]; then
+    echo "capture_dir cannot be /"
+    exit 1
+fi
+
+if [ $(readlink -f $PROCESS_DIR) == "/" ]; then
+    echo "process_dir cannot be /"
+    exit 1
+fi
+
+#
+# Capture packets using tcpdump. Move rotated pcap files to process_dir.
+#
 ROTATE_SEC=2
 SNARF_LEN=58
 
-/usr/bin/mkdir -p "$CAPTURE_DIR"
-/usr/bin/chmod 777 "$CAPTURE_DIR"
-/usr/bin/tcpdump -i "$INTERFACE" -G $ROTATE_SEC -w "$CAPTURE_DIR/%s.pcap" -s $SNARF_LEN
+# If the accumulator isn't keeping up with the captured files, the process
+# directory can get really big. It can get so big even listing the contents
+# is slow. To handle this problem, we'll have a size threshold
+# of the process directory in bytes, which, if exceeded, causes the directory
+# to be cleared out.
+# NOTE: tmpfs has small empty directory sizes relative to, say, ext4 (60 bytes vs 4096
+# on my test system), so this number has to be experimentally set.
+MAX_PROCESS_DIR_SIZE=10000
+
+# tcpdump -z option only allows commands of the format "command <file>", so we need to capture
+# the directory to move to somehow. We will accomplish this by creating a temporary script.
+MOVE_SCRIPT=$(mktemp "/tmp/move_script.XXXXX")
+chmod 700 "$MOVE_SCRIPT"
+cat >"$MOVE_SCRIPT"<<EOF
+#!/bin/bash
+set -e
+mkdir -p "$PROCESS_DIR"
+chmod 777 "$PROCESS_DIR"
+if [ \$(stat --format="%s" "$PROCESS_DIR") -gt $MAX_PROCESS_DIR_SIZE ]; then
+    echo "$PROCESS_DIR has too many entries. Cleanup up."
+    find "$PROCESS_DIR" -type f -name '*.pcap' -delete;
+fi
+mv "\$1" "$PROCESS_DIR"
+echo "Moved \$1 to $PROCESS_DIR"
+EOF
+
+trap "rm \"$MOVE_SCRIPT\"" SIGINT SIGTERM
+
+mkdir -p "$CAPTURE_DIR"
+tcpdump -i "$INTERFACE" -G $ROTATE_SEC -s $SNARF_LEN -w "$CAPTURE_DIR/%s.pcap" -z "$MOVE_SCRIPT"
 
