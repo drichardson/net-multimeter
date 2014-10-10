0540dba536f003e0805c19cb7a3d9f45f60ff917
Doug Richardson <dougie.richardson@gmail.com>
2014-10-10 04:37:54 -0700

    Don't cache accumulator.json in nginx; display some by ip addresses in a table.

--- html/js/main.js
+++ html/js/main.js
@@ -1,5 +1,5 @@
 require(["jquery", "poll"], function($, poll) {
-    // $.noConflict(true);
+    //$.noConflict(true);
 
     var RATE_LIMIT_MS = 2000;
     var poller = poll.Poller("accumulator.json", RATE_LIMIT_MS, jsonLoaded);
@@ -8,8 +8,25 @@ require(["jquery", "poll"], function($, poll) {
         poller.start();
     }
 
-    function jsonLoaded(data, status, jqXHR) {
-        console.log("JSON data loaded: " + data);
+    var jel = function(e) { return $(document.createElement(e)); }
+    var td = function(text) { return jel("td").text(text); }
+
+    function jsonLoaded(data) {
+        var a = data["ip_by_address_pair"];
+        var l = a.length;
+        console.log("len " + l);
+        var i;
+        var tbody = $("#by_ip");
+        tbody.html("")
+        console.log("body: " + tbody);
+        for(i = 0; i < l; ++i) {
+            console.log("i is " + i);
+            var c = a[i];
+            var td1 = td(c["high-address"] + "<->" + c["low-address"]);
+            var td2 = td("");
+            var td3 = td("");
+            tbody.append($("<tr>"), td1, td2, td3);
+        }
     }
 
     $(document).ready(main);
